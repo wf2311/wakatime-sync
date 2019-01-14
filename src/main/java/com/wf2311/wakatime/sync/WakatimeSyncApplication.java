a3419321e4ab9fee5a80970f90b7fd81f38ca371
wf2311 <wf2311@163.com>
2019-01-14 11:45:55 +0800

    删除无用代码

--- src/main/java/com/wf2311/wakatime/sync/WakatimeSyncApplication.java
+++ src/main/java/com/wf2311/wakatime/sync/WakatimeSyncApplication.java
@@ -4,19 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
 @SpringBootApplication
 @EnableScheduling
-//@EntityScan(basePackageClasses =
-//        {WakatimeSyncApplication.class,
-//                Jsr310JpaConverters.class})
 public class WakatimeSyncApplication {
 
     public static void main(String[] args) {
-        System.out.println(LocalDate.now());
-        System.out.println(LocalDateTime.now());
         SpringApplication.run(WakatimeSyncApplication.class, args);
     }
 
